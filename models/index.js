d218207b3cb2483b2204bb786cfb36793fde3955
Lily Russ <llyruss@yahoo.com>
2022-11-25 14:19:52 -0600

    productTag model, tag model, index model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+    through:{
+      model: ProductTag,
+      unique: false
+    },
+});
 
 module.exports = {
   Product,
